4f775bd1ebf4307c9bb0338c0b8ef92b7821828c
shoabul <hello.shoabul@gmail.com>
2026-09-13 22:32:38 +0600

    fix: ass to stack button , comment disabled={isAdded}

--- src/components/stack/StackCard.jsx
+++ src/components/stack/StackCard.jsx
@@ -48,7 +48,7 @@ export default function StackCard({ tech, onAdd, isAdded }) {
 
                 <button
                     onClick={() => onAdd(tech)}
-                    disabled={isAdded}
+                    // disabled={isAdded}
                     className={`w-full py-3 text-sm font-semibold rounded-xl transition-all ${isAdded
                         ? "bg-slate-100 text-slate-400"
                         : "bg-slate-950 hover:bg-slate-800 text-white cursor-pointer"
